142a71ece7fda6176dc00a765447d300cb470561
Kaloyan9905 <106106437+Kaloyan9905@users.noreply.github.com>
2022-08-02 23:03:37 +0300

    Add files via upload

---
+++ functions/Add and Subtract.py
@@ -0,0 +1,9 @@
+def final_answer(first, second, third):
+    result = first + second - third
+    return result
+
+
+number1 = int(input())
+number2 = int(input())
+number3 = int(input())
+print(final_answer(number1, number2, number3))
