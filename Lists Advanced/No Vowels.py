7da2515139e5242f6c4321bce106fc65e2553499
Kaloyan9905 <106106437+Kaloyan9905@users.noreply.github.com>
2022-08-09 21:02:55 +0300

    Add files via upload

---
+++ Lists Advanced/No Vowels.py
@@ -0,0 +1,7 @@
+data = input()
+new_data = data.replace('a', '')
+new_data1 = new_data.replace('u', '')
+new_data2 = new_data1.replace('o', '')
+new_data3 = new_data2.replace('e', '')
+new_data4 = new_data3.replace('i', '')
+print(new_data4)
